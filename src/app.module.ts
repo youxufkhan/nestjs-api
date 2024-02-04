6ce773e9e2b76737bb11f027cac50772a9016178
Yousuf Khan <m.youxuf@gmail.com>
2024-02-04 21:26:07 +0500

    mikro orm setup wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ItemsModule } from './items/items.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 @Module({
-  imports: [],
+  imports: [ MikroOrmModule.forRoot({
+    entities: ['./dist/entities'],
+    entitiesTs: ['./src/entities'],
+    dbName: 'my-database',
+    type: 'postgresql',
+  }), 
+  ItemsModule
+],
   controllers: [AppController],
   providers: [AppService],
 })

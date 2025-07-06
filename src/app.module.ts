3e195519a081cfbd4bcdff8d8dedf43233fe5a7a
Yousuf Khan <m.youxuf@gmail.com>
2025-07-06 16:44:42 +0500

    repo init with new nest version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+
 @Module({
-  imports: [ MikroOrmModule.forRoot({
-    entities: ['./dist/entities'],
-    entitiesTs: ['./src/entities'],
-    dbName: 'my-database',
-    type: 'postgresql',
-  }), 
-  ItemsModule
-],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
